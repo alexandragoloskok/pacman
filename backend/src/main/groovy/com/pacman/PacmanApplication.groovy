c0399155a259b17b58f93d0632be59cb35eb8577
alexandragoloskok <alexandragoloskok@gmail.com>
2019-07-26 17:53:34 +0300

    back

---
+++ backend/src/main/groovy/com/pacman/PacmanApplication.groovy
@@ -0,0 +1,13 @@
+package com.pacman
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class PacmanApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(PacmanApplication, args)
+    }
+
+}
